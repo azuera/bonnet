b7328b49f6f061b0c1eeed41e9709d58be35d553
azuera <terminatur45@gmail.com>
2023-04-18 15:56:53 +0200

    ajout refacto class cart

--- pages/list.php
+++ pages/list.php
@@ -2,81 +2,84 @@
 
 
 $produitsFiltred = $produits;
-$prixMax=null;
-$prixMini=null;
+$prixMax = null;
+$prixMini = null;
 $material = null;
-$size=null;
+$size = null;
 
 
-
-if(!empty($_POST['prix-mini'])){
+if (!empty($_POST['prix-mini'])) {
     $prixMini = floatval($_POST['prix-mini']);
 
     $produitsFiltred = array_filter($produitsFiltred, function (Produit $produit) use ($prixMini) {
-        return $produit->getPrix()>= $prixMini;
+        return $produit->getPrix() >= $prixMini;
     });
 }
-if(!empty($_POST['prix-max'])){
+if (!empty($_POST['prix-max'])) {
     $prixMax = floatval($_POST['prix-max']);
 
-    $produitsFiltred = array_filter($produitsFiltred, function (Produit $produit) use ($prixMax){
-        return $produit->getPrix()<= $prixMax;
+    $produitsFiltred = array_filter($produitsFiltred, function (Produit $produit) use ($prixMax) {
+        return $produit->getPrix() <= $prixMax;
     });
 }
-if(!empty($_POST['material'])) {
+if (!empty($_POST['material'])) {
     $material = trim($_POST['material']);
 
     $produitsFiltred = array_filter($produitsFiltred, function (Produit $produit) use ($material) {
 
-        return in_array($material,$produit->getMaterials());
+        return in_array($material, $produit->getMaterials());
     });
 }
-if(!empty($_POST['size'])){
+if (!empty($_POST['size'])) {
     $size = trim($_POST['size']);
 
-    $produitsFiltred = array_filter($produitsFiltred, function (Produit $produit) use ($size){
+    $produitsFiltred = array_filter($produitsFiltred, function (Produit $produit) use ($size) {
 
-        return in_array($size,$produit->getSizes());
+        return in_array($size, $produit->getSizes());
     });
 }
 ?>
 <form action='' method="post">
-<div class="mb-3">
-  <label for="prix-mini" class="form-label">prix-mini</label>
-  <input type="number" class="form-control" id="prix-mini" name="prix-mini" value="<?=$prixMini;?>">
-</div>
-  <div class="mb-3">
-  <label for="prix-max" class="form-label">prix-maxi</label>
-  <input type="number" class="form-control" id="prix-max" name="prix-max" value="<?=$prixMax;?>" >
-</div>
-<div class="mb-3">
-  <label for="material" class="form-label">matiere</label>
-  <select  id="material" name="material" >
-    <option value=""> choisissez votre matiere</option>
-    <?php 
-    foreach (Produit::AVAILABLE_MATERIALS as $value => $name) {
-        ?>
-        <option value="<?=$name;?>" <?php if($name == $material) {echo'selected';}?>><?php  echo $name;?></option>
-     <?php   
-    }
-    ?>
-  </select>
-</div>
+    <div class="mb-3">
+        <label for="prix-mini" class="form-label">prix-mini</label>
+        <input type="number" class="form-control" id="prix-mini" name="prix-mini" value="<?= $prixMini; ?>">
+    </div>
+    <div class="mb-3">
+        <label for="prix-max" class="form-label">prix-maxi</label>
+        <input type="number" class="form-control" id="prix-max" name="prix-max" value="<?= $prixMax; ?>">
+    </div>
+    <div class="mb-3">
+        <label for="material" class="form-label">matiere</label>
+        <select id="material" name="material">
+            <option value=""> choisissez votre matiere</option>
+            <?php
+            foreach (Produit::AVAILABLE_MATERIALS as $value => $name) {
+                ?>
+                <option value="<?= $name; ?>" <?php if ($name == $material) {
+                    echo 'selected';
+                } ?>><?php echo $name; ?></option>
+                <?php
+            }
+            ?>
+        </select>
+    </div>
     <div class="mb-3">
         <label for="size" class="form-label">size</label>
-        <select  id="size" name="size" >
+        <select id="size" name="size">
             <option value=""> choisissez votre taille</option>
             <?php
             foreach (Produit::AVAILABLE_SIZES as $value => $name) {
                 ?>
-                <option value="<?=$name;?>" <?php if($name == $size){echo'selected';}?>><?php  echo $name;?></option>
+                <option value="<?= $name; ?>" <?php if ($name == $size) {
+                    echo 'selected';
+                } ?>><?php echo $name; ?></option>
                 <?php
             }
             ?>
         </select>
     </div>
 
-  <button type="submit" class="btn btn-primary">GO ENVOI</button>
+    <button type="submit" class="btn btn-primary">GO ENVOI</button>
 </form>
 
 <table>
@@ -93,16 +96,16 @@ if(!empty($_POST['size'])){
         <th>
             desc
         </th>
-        
+
         <th>
             panier
         </th>
     </tr>
 
 
-            <?php foreach ($produitsFiltred as  $produit){ 
-                displayBonnet ($produit);
-} ?>
+    <?php foreach ($produitsFiltred as $produit) {
+        displayBonnet($produit);
+    } ?>
 
 </table>
 
---
+++ src/Cart.php
@@ -0,0 +1,108 @@
+<?php
+
+class Cart
+{
+    protected $content = [];
+
+    public function __construct()
+    {
+        if (!isset($_SESSION['cart'])) {
+            $_SESSION['cart'] = [];
+        }
+        $this->content = $_SESSION['cart'];
+    }
+
+    public function getContent(): array
+    {
+        return $this->content;
+    }
+
+    public function setContent(array $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function plus(int $index): self
+    {
+
+        if (!isset($this->content[$index])) {
+            $this->content[$index] = 0;
+        }
+        $this->content[$index]++;
+        return $this;
+    }
+
+    public function min(int $index): self
+    {
+
+        if (isset($this->content[$index])) {
+            $this->content[$index]--;
+
+        }
+
+        if ($this->content[$index] <= 0) {
+            unset($this->content[$index]);
+        }
+        return $this;
+    }
+
+    public function delete(int $index): self
+    {
+        if (isset($this->content[$index])) {
+            unset($this->content[$index]);
+        }
+
+        return $this;
+    }
+
+    public function  empty(): self
+    {
+        $this->content = [];
+        return $this;
+    }
+
+    public function save(): self
+    {
+        $_SESSION['cart'] = $this->getContent();
+        return $this;
+    }
+/* $getdata = $_GET cest just plus propre!!*/
+    public function handle(array $getData): bool
+    {
+        if (isset($getData['index'])) {
+            $index = $getData['index'];
+
+
+            $mode = 'plus';
+            if (isset($getData['mode'])) {
+                $mode = $getData['mode'];
+            }
+
+
+            switch ($mode) {
+                case 'plus':
+                    $this->plus($index);
+
+                    break;
+                case 'min':
+                    $this->min($index);
+
+                    break;
+                case 'delete':
+                    $this->delete($index);
+                    break;
+
+            }
+            $this->save();
+
+            return true;
+        }
+        if (isset($getData['mode']) && $getData['mode'] == 'empty') {
+            $this->empty();
+            $this->save();
+            return true;
+        }
+        return false;
+    }
+}
+
